b4d1d5699a804df6523af886a4e6593d48977992
Lambo1012 <ahmedovmirzomuhammad958@gmail.com>
2026-09-14 15:19:56 +0300

    Первая задача

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <numbers>
+#include <math.h>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+
+    double r;
+    cout << "Введите радиус: ";
+    cin >> r;
+
+    double C = 2 * numbers::pi * r;      // дл.окр
+    double S = numbers::pi * r * r;      // пл.круга
+
+    cout << "Длина окружности: " << C << endl;
+    cout << "Площадь круга: " << S << endl;
+}
